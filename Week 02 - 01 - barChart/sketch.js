b3be42bbef6be5aa73b894f16e2de635fca1da86
Sonia Ndonga <soniandonga@yahoo.com>
2022-02-07 16:27:45 +0000

    Created barCharts implemented code

---
+++ Week 02 - 01 - barChart/sketch.js
@@ -0,0 +1,41 @@
+//p5 Functions needs to be defined in the setup() or draw()
+
+let chartWidth = 400;
+let chartHeight = 400;
+let data = [345, 123, 120, 233, 345, 123, 120, 233, 345, 123, 120, 233];
+let margin = 20;
+let spacing = 1;
+
+let remaingSpace = chartWidth - margin * 2 - spacing * (data.length - 1);
+
+let barWidth = remaingSpace / data.length;
+
+console.log(barWidth);
+
+//The setup function only excutes the code once
+function setup() {
+  createCanvas(500, 500);
+  background(255); // sets to specify colour //Defining a function
+}
+
+//The draw function can excute the code a number of times
+function draw() {
+  background(255);
+
+  translate(50, 400);
+  stroke(0);
+  strokeWeight(3);
+  //Y axis
+  line(0, 0, 0, -400);
+  //X axis
+  line(0, 0, 400, 0);
+
+  noStroke();
+
+  translate(margin, 0);
+  for (let i = 0; i < data.length; i++) {
+    rect(i * (barWidth + spacing), 0, barWidth, -data[i]);
+  }
+
+  fill(0, 0, 255);
+}
